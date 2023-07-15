009c3387eecd54dbd1aa65b3ada66cd94cb0a70f
Jose8818 <jose.guerra.aguinaga@gmail.com>
2023-07-15 13:30:42 -0600

    Se aggrego test

---
+++ test.py
@@ -0,0 +1,16 @@
+import app
+import geocoder
+
+def test():
+    ciudad = "Apodaca"
+    g = geocoder.osm(ciudad)
+    latitud, longitud = g.latlng
+
+    resultado = app.obtener_clima(latitud, longitud)
+
+    if resultado is None:
+        print("Error: El resultado es nulo")
+    else:
+        print("El resultado es:", resultado)
+
+test()
